eb7f49b23e3df5a3fe447082440967a9b078e1bd
GruzdovAN <antologyz@gmail.com>
2016-02-21 05:15:47 +0300

    create class GameManager, transfer him function draw fron class CanvasView (reverted from commit c96e3ec651397b6409b21efbd9a89ff32e444ad0)

--- src/Art/AgarioForAndroid/CanvasView.java
+++ src/Art/AgarioForAndroid/CanvasView.java
@@ -2,6 +2,7 @@ package Art.AgarioForAndroid;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -10,21 +11,30 @@ import android.view.View;
  */
 public class CanvasView extends View {
 
-
-    private GameManager gameManager;
+    private MainCircle mainCircle;
+    private Paint paint;
     
 
     public CanvasView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        gameManager = new GameManager();
-
+        initMainCircle();
+        initPoint();
     }
 
+    private void initPoint() {
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.FILL);
+    }
 
+    private void initMainCircle() {
+
+        mainCircle = new MainCircle(200, 500);
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        gameManager.onDraw(canvas);
+        canvas.drawCircle(mainCircle.getX(),mainCircle.getY(),mainCircle.getReadius(),paint);
     }
 }
