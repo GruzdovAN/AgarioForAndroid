327705f84f399bce7629335a3f7d22aadd6846cf
GruzdovAN <antologyz@gmail.com>
2016-02-21 08:09:47 +0300

    refactoring action draw circle

--- src/Art/AgarioForAndroid/ICanvasView.java
+++ src/Art/AgarioForAndroid/ICanvasView.java
@@ -4,5 +4,5 @@ package Art.AgarioForAndroid;
  * Created by Elkin on 21.02.2016.
  */
 public interface ICanvasView {
-    void drawCircle(MainCircle circle);
+    void drawCircle(SimpleCircle circle);
 }
---
+++ src/Art/AgarioForAndroid/SimpleCircle.java
@@ -0,0 +1,33 @@
+package Art.AgarioForAndroid;
+
+/**
+ * Created by Elkin on 21.02.2016.
+ */
+public class SimpleCircle {
+    protected int x;
+    protected int y;
+    protected int radius;
+
+
+
+    public SimpleCircle(int x, int y,int radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getReadius() {
+        return radius;
+    }
+
+
+
+}
