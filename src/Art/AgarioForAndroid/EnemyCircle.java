2af3577e9ac5954d70f77e7a44127154f665c36b
GruzdovAN <antologyz@gmail.com>
2016-02-23 00:27:56 +0300

    moveEnemyAndFoodCircle

--- src/Art/AgarioForAndroid/EnemyCircle.java
+++ src/Art/AgarioForAndroid/EnemyCircle.java
@@ -10,12 +10,17 @@ import java.util.Random;
 public class EnemyCircle extends SimpleCircle {
 
     public static final int FROM_RADIUS = 10;
+    public static final int RANDOM_SPEED = 10;
     public static final int TO_RADIUS = 110;
     public static final int ENEMY_COLOR = Color.RED;
     public static final int FOOD_COLOR = Color.rgb(0, 200, 0);
+    private int dx;
+    private int dy;
 
-    public EnemyCircle(int x, int y, int radius) {
+    public EnemyCircle(int x, int y, int radius, int dx, int dy) {
         super(x, y, radius);
+        this.dx = dx;
+        this.dy = dy;
     }
 
 
@@ -23,8 +28,10 @@ public class EnemyCircle extends SimpleCircle {
         Random random = new Random();
         int x = random.nextInt(GameManager.getWidth());
         int y = random.nextInt(GameManager.getHeight());
+        int dx = 1 + random.nextInt(RANDOM_SPEED);
+        int dy = 1 + random.nextInt(RANDOM_SPEED);
         int radius = 10 + random.nextInt(TO_RADIUS - FROM_RADIUS);
-        EnemyCircle enemyCircle = new EnemyCircle(x, y,radius);
+        EnemyCircle enemyCircle = new EnemyCircle(x, y,radius, dx, dy);
         enemyCircle.setColor(ENEMY_COLOR);
         return enemyCircle;
     }
@@ -44,4 +51,9 @@ public class EnemyCircle extends SimpleCircle {
         return false;
     }
 
+    public void moveOneStep() {
+
+        x += dx;
+        y += dy;
+    }
 }
